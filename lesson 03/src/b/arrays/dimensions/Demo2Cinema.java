6502ee7ab9eab70f7b1d9415b1247e91a4623412
jbt <jbt@405_INS>
2021-06-07 14:48:03 +0300

    aaa

--- lesson 03/src/b/arrays/dimensions/Demo2Cinema.java
+++ lesson 03/src/b/arrays/dimensions/Demo2Cinema.java
@@ -1,26 +1,37 @@
 package b.arrays.dimensions;
 
+import java.util.Scanner;
+
 public class Demo2Cinema {
 
 	public static void main(String[] args) {
 
 		char[][] cinema = new char[3][5];
-		populate(cinema);
-		print(cinema);
-
-	}
-
-	/** fill the matrix with empty seats - O character */
-	public static void populate(char[][] cinema) {
+		// populate with O characters
 		for (int i = 0; i < cinema.length; i++) {
 			for (int j = 0; j < cinema[i].length; j++) {
 				cinema[i][j] = 'O';
 			}
 		}
-	}
 
-	/** print the matrix */
-	public static void print(char[][] cinema) {
+		// print the cinema
+		for (int i = 0; i < cinema.length; i++) {
+			for (int j = 0; j < cinema[i].length; j++) {
+				System.out.print(" " + cinema[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		// get input from user
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter row: ");
+		int row = sc.nextInt() - 1;
+		System.out.print("enter seat: ");
+		int seat = sc.nextInt() - 1;
+
+		cinema[row][seat] = 'X';
+
+		// print the cinema
 		for (int i = 0; i < cinema.length; i++) {
 			for (int j = 0; j < cinema[i].length; j++) {
 				System.out.print(" " + cinema[i][j] + " ");
