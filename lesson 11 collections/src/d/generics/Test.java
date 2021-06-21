425f42ff5e014fa48e2be842d29dc88f955dbb58
jbt <jbt@405_INS>
2021-06-21 12:03:47 +0300

    ccc

--- lesson 11 collections/src/d/generics/Test.java
+++ lesson 11 collections/src/d/generics/Test.java
@@ -6,9 +6,11 @@ public class Test {
 
 		Box<String> boxOfString = new Box<>();
 		boxOfString.setContent("aaa");
+		System.out.println(boxOfString.getContent());
 
 		Box<Integer> boxOfInteger = new Box<>();
 		boxOfInteger.setContent(5);
+		System.out.println(boxOfInteger.getContent());
 	}
 
 }
---
+++ lesson 11 collections/src/e/sort/Demo1.java
@@ -0,0 +1,45 @@
+package e.sort;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		{
+			List<Integer> list = Arrays.asList(4, 5, 8, 2, 5, 3, 1);
+			System.out.println(list);
+			list.sort(null);
+			System.out.println(list);
+		}
+		System.out.println("============================");
+		{
+			List<String> list = Arrays.asList("banana", "dog", "cat", "apple");
+			System.out.println(list);
+			list.sort(null);
+			System.out.println(list);
+		}
+		System.out.println("============================");
+		{
+			List<LocalDate> list = Arrays.asList(
+
+					LocalDate.of(1980, 5, 25),
+
+					LocalDate.of(2021, 5, 25),
+
+					LocalDate.of(1980, 3, 25),
+
+					LocalDate.of(800, 5, 25),
+
+					LocalDate.of(2000, 5, 25)
+
+			);
+			System.out.println(list);
+			list.sort(null);
+			System.out.println(list);
+		}
+		System.out.println("============================");
+	}
+}
