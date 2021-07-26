3aa73f33db47d1853252cd115068b45ec56b0cc1
jbt <jbt@405_INS>
2021-07-26 13:12:41 +0300

    aaa

--- spring-rest-controller/src/main/java/app/core/controllers/EmployeeController.java
+++ spring-rest-controller/src/main/java/app/core/controllers/EmployeeController.java
@@ -3,14 +3,19 @@ package app.core.controllers;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.core.entities.Employee;
 import app.core.services.EmpService;
 
 @RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
 
 	private EmpService empService;
@@ -21,9 +26,21 @@ public class EmployeeController {
 	}
 
 	// map this method so it has a URL
-	@RequestMapping(method = RequestMethod.GET, path = "/a", produces = MediaType.APPLICATION_XML_VALUE)
+	@GetMapping(path = "/all", produces = { "application/json", "application/xml" })
 	public List<Employee> getEmployees() {
 		return this.empService.getAllEmps();
+	}
+
+	@GetMapping("/one")
+	public Employee getEmployee(@RequestParam long id) {
+		return this.empService.getEmployee(id);
+	}
+
+	// url: http://localhost:8080/api/employee/add
+	// body: {"name":"John", "role":"Manager"}
+	@PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public long addEmployee(@RequestBody Employee emp) {
+		return this.empService.addEmployee(emp);
 	}
 
 }
