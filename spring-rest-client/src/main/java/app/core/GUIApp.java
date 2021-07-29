5f99d411b21ee3d1c786b6b0c7de58bc886fb757
jbt <jbt@405_INS>
2021-07-29 11:41:26 +0300

    html

--- spring-rest-client/src/main/java/app/core/GUIApp.java
+++ spring-rest-client/src/main/java/app/core/GUIApp.java
@@ -29,22 +29,42 @@ public class GUIApp {
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JLabel lbId = new JLabel("employee id");
-		lbId.setBounds(25, 25, 150, 25);
+		lbId.setBounds(25, 20, 150, 25);
 		fr.add(lbId);
 
 		JTextField tfId = new JTextField();
-		tfId.setBounds(200, 25, 150, 25);
+		tfId.setBounds(200, 20, 150, 25);
 		fr.add(tfId);
 
-		JButton btSend = new JButton("Send");
-		btSend.setBounds(25, 90, 150, 25);
-		fr.add(btSend);
+		JLabel lbName = new JLabel("employee name");
+		lbName.setBounds(25, 50, 150, 25);
+		fr.add(lbName);
+
+		JTextField tfName = new JTextField();
+		tfName.setBounds(200, 50, 150, 25);
+		fr.add(tfName);
+
+		JLabel lbRole = new JLabel("employee role");
+		lbRole.setBounds(25, 80, 150, 25);
+		fr.add(lbRole);
+
+		JTextField tfRole = new JTextField();
+		tfRole.setBounds(200, 80, 150, 25);
+		fr.add(tfRole);
+
+		JButton btRead = new JButton("Read");
+		btRead.setBounds(25, 110, 70, 25);
+		fr.add(btRead);
+
+		JButton btAdd = new JButton("Add");
+		btAdd.setBounds(125, 110, 70, 25);
+		fr.add(btAdd);
 
 		JLabel lbDisp = new JLabel();
-		lbDisp.setBounds(25, 130, 300, 25);
+		lbDisp.setBounds(25, 140, 300, 25);
 		fr.add(lbDisp);
 
-		btSend.addActionListener(new ActionListener() {
+		btRead.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -55,6 +75,32 @@ public class GUIApp {
 					String url = "http://localhost:8080/api/employee/find-one?empId=" + id;
 					Employee emp = rt.getForObject(new URI(url), Employee.class);
 					lbDisp.setText(emp.toString());
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+				} catch (RestClientException e2) {
+					lbDisp.setText(e2.getMessage());
+				} catch (NumberFormatException e2) {
+					lbDisp.setText("Bad Input: " + e2.getMessage());
+				}
+
+			}
+		});
+
+		btAdd.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				try {
+					// on click
+					tfName.setText(null);
+					tfRole.setText(null);
+					String name = tfId.getText();
+					String role = tfRole.getText();
+					String url = "http://localhost:8080/api/employee/add";
+					Employee emp = new Employee(name, role);
+					long id = rt.postForObject(new URI(url), emp, Long.class);
+					lbDisp.setText("id: " + id);
 				} catch (URISyntaxException e1) {
 					e1.printStackTrace();
 				} catch (RestClientException e2) {
