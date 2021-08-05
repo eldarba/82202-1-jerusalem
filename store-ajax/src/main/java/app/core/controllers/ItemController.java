00330a64daede91e296e6fa1558ef093087205ff
jbt <jbt@405_INS>
2021-08-05 15:24:39 +0300

    aaa

--- store-ajax/src/main/java/app/core/controllers/ItemController.java
+++ store-ajax/src/main/java/app/core/controllers/ItemController.java
@@ -3,6 +3,7 @@ package app.core.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import app.core.beans.Item;
 
@@ -39,7 +41,8 @@ public class ItemController {
 		if (index != -1) {
 			return this.items.get(index);
 		} else {
-			return null;
+			String msg = "item with id " + itemId + " not found";
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
 		}
 	}
 
